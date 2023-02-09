7ae7b2785c5a07d29cf5e017516d84645d29f6e6
Anton Abramov <blacknoirxd@icloud.com>
2023-02-09 14:56:31 +0500

    Add tableView to ResultVC
    Add tableView to ResultVC

--- WeatherApp/Main module/ViewController/ResultViewController.swift
+++ WeatherApp/Main module/ViewController/ResultViewController.swift
@@ -1,11 +1,46 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-    let tableView = UITableView(frame: CGRect(), style: .grouped)
+    let tableView = UITableView(frame: CGRect(), style: .plain)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(ResultTableViewCell.nib(), forCellReuseIdentifier: ResultTableViewCell.identifire)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableViewSetConstraints()
+    }
+    
+    //MARK: tableViewConstraints
+    func tableViewSetConstraints() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        tableView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 
+}
+
+// MARK: - UITableViewDataSource
+extension ResultViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ResultTableViewCell.identifire, for: indexPath) as? ResultTableViewCell else { return UITableViewCell() }
+        return cell
+    }
+    
+}
+
+// MARK: - UITableViewDelegate
+extension ResultViewController: UITableViewDelegate {
+    
 }
